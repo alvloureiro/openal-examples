7bc3d76694269efbc8b12ccc3f135ec7b8e5b86f
Andre Loureiro <loureiro.andrew@gmail.com>
2015-03-16 07:25:49 +0900

    Loading and playing a sample audio file
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- openal-example-1/main.c
+++ openal-example-1/main.c
@@ -1,31 +1,145 @@
 #include <AL/al.h>
+#include <AL/alc.h>
 #include <AL/alut.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static const char *audio_file = "../data/wave/audio.wav";
+const char *audio_file = "sample1.wav";
+static const int capture_audio_freq = 44100;
+static const int capture_audio_size = 1024;
+
+ALbyte buffer[22050];
+ALint sample;
+ALCdevice *g_device = NULL;
+ALCcontext *g_context = NULL;
+ALCenum g_error = AL_NO_ERROR;
+const ALchar *g_string_error = NULL;
+
+static ALboolean check_al_error()
+{
+    g_error = alGetError();
+    if (g_error != AL_NO_ERROR && alGetString(g_error) != NULL) {
+        g_string_error = alGetString(g_error);
+        printf("AL error: %s\n", g_string_error);
+        g_error = AL_NO_ERROR;
+        g_string_error = NULL;
+        return AL_TRUE;
+    }
+    return AL_FALSE;
+}
+
+static ALboolean check_alut_error()
+{
+    g_error = alutGetError();
+    if (g_error != ALUT_ERROR_NO_ERROR) {
+        g_string_error = alutGetErrorString(g_error);
+        printf("ALUT error: %s\n", g_string_error);
+        g_error = ALUT_ERROR_NO_ERROR;
+        g_string_error = NULL;
+        return AL_TRUE;;
+    }
+    return AL_FALSE;
+}
+
+static void initialize_audio_library ()
+{
+    alGetError();
+    if (g_device == NULL) {
+        // getting the default device
+        g_device = alcOpenDevice(NULL);
+        check_al_error();
+    }
+    if (g_device != NULL) {
+        g_context = alcCreateContext(g_device, NULL);
+        if (g_context != NULL) {
+            if (alcMakeContextCurrent(g_context) == AL_FALSE) {
+                printf("Failed to make context current\n");
+                check_al_error();
+                return;
+            }
+        }
+    }
+}
+
+static void close_audio_library ()
+{
+    if (g_context != NULL) {
+        // release the context before destroy it
+        alcMakeContextCurrent(NULL);
+        alcDestroyContext(g_context);
+    }
+    if (g_device != NULL) {
+        // close the current device
+        alcCloseDevice(g_device);
+    }
+}
+
+static void print_available_devices ()
+{
+    const ALCchar *available_devices = NULL;
+    const ALCchar *next = NULL;
+    size_t length = 0;
+
+    available_devices = alcGetString(NULL, ALC_CAPTURE_DEVICE_SPECIFIER);
+    next = available_devices + 1;
+    //XXX do it using strtok
+    printf("Available capture devices\n");
+    while (available_devices && *available_devices != '\0'
+            && next && *next != '\0') {
+        printf("Device: %s\n", available_devices);
+        length = strlen(available_devices);
+        available_devices += (length + 1);
+        next += (length + 2);
+    }
+}
+
+static void start_record_audio ()
+{
+    print_available_devices();
+    ALCdevice *capture_device = alcCaptureOpenDevice(NULL, capture_audio_freq, AL_FORMAT_STEREO16, capture_audio_size);
+    ALboolean got_error = check_al_error();
+    if (got_error) {
+        printf("Error when try to open a capture device\n");
+        alcCaptureCloseDevice(capture_device);
+        return;
+    }
+    alcCaptureStart(capture_device);
+    int capturing = 1;
+    while (capturing) {
+        alcGetIntegerv(capture_device, ALC_CAPTURE_SAMPLES, (ALsizei) sizeof(ALint), &sample);
+    }
+    do {
+        printf("sample size: %d\n", sample);
+    } while (sample < 40960);//22050); // 4096 = 1024 * 4 comes from AL_FORMAT_STEREO16
+
+    alcCaptureSamples(capture_device, (ALCvoid *)buffer, sample);
+    alcCaptureStop(capture_device);
+    alcCaptureCloseDevice(capture_device);
+}
+
+static void stop_record_audio ()
+{
+    return;
+}
 
 int main (int argc, char** argv)
 {
-    ALenum error;
     ALuint buffer;
     ALuint source, source_state;
 
-    alutGetError();
+    initialize_audio_library();
 
     alutInit(&argc, argv);
-    error = alutGetError();
-    if (error != ALUT_ERROR_NO_ERROR) {
-        printf("Error when try initialize alut context: %s", alutGetErrorString(error));
+    if (check_alut_error()) {
         return EXIT_FAILURE;
     }
 
-    alutGetError();
     buffer = alutCreateBufferFromFile(audio_file);
-    if (buffer == AL_NONE) {
-        error = alutGetError();
-        printf("Error when try to load audio file: %s", alutGetErrorString(error));
+    if (check_alut_error()) {
+        return EXIT_FAILURE;
+    } else if (buffer == AL_NONE) {
+        close_audio_library();
         alutExit();
         return EXIT_FAILURE;
     }
@@ -50,5 +164,7 @@ int main (int argc, char** argv)
         alGetSourcei(source, AL_SOURCE_STATE, &source_state);
     }
 
+    close_audio_library();
+    alutExit();
     return EXIT_SUCCESS;
 }
